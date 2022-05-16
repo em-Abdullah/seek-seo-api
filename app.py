c051a36281ca93b66daa10ae6156093cbcccd20f
Muhammad Ali <amuhammad161@gmail.com>
2022-05-16 16:56:40 +0500

    added files

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, make_response, render_template
-from seoanalyzer import analyze
+from analyzer import analyze
 from flask import request
 from flask import url_for
+from bs4 import BeautifulSoup
+from operator import itemgetter
 
 
 app = Flask(__name__)
@@ -12,6 +14,7 @@ def search():
     if request.method == 'GET':
         args = request.args
         content = args.get('url')
-    output = make_response(analyze(content, follow_links=False))
+    output = make_response(analyze(
+        content, follow_links=False, analyze_headings=True, analyze_extra_tags=True))
     output.headers['Access-Control-Allow-Origin'] = '*'
     return output
